41c53f5ccb2f05f1ea4a6d19e9e191ceb0b5a0ab
BrunoGoretti <104521859+BrunoGoretti@users.noreply.github.com>
2023-02-06 18:59:21 +0400

    Add data context

---
+++ ChiliLabsHomework/Data/UserContext.cs
@@ -0,0 +1,12 @@
+﻿using ChiliLabsHomework.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChiliLabsHomework.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+        : base(options) { }
+        public DbSet<UserModel> DbUsers { get; set; }
+    }
+}
--- ChiliLabsHomework/Models/UserModel.cs
+++ ChiliLabsHomework/Models/UserModel.cs
@@ -6,6 +6,12 @@ namespace ChiliLabsHomework.Models
     {
         [Key]
         public int UserId { get; set; }
-
+        public string Identifier { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AvatarUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
